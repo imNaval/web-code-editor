e89c6f1bd3ecabca1e4cb72d13630ad78de07aa0
imNaval <nehranvl2017@gmail.com>
2023-08-07 22:47:29 +0530

    expand-collapse

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/css/css'
 import 'codemirror/mode/javascript/javascript'
 import {Controlled as ControlledEditor} from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 
 const Editor = (props) => {
@@ -15,14 +17,23 @@ const Editor = (props) => {
         onChange
     } = props;
 
+    const [open, setOpen] = useState(true)
+
     function handleChange(editor, data, value){
         onChange(value);
     }
+
   return (
-    <div className='editor-container'>
+    <div className={`editor-container ${!open && 'collapsed' }`}>
         <div className='editor-title'>
             {displayName}
-            <button>O/C</button>
+            <button 
+                type='button'
+                className='expand-collapse-btn'
+                onClick={()=>setOpen(prev => !prev)}
+            >
+                <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+            </button>
         </div>
 
         <ControlledEditor 
