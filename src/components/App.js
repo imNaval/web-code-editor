6abdc1416c6a458e1eeeecf7b34095be642450b0
imNaval <nehranvl2017@gmail.com>
2023-08-10 22:22:00 +0530

    add clear and save features

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import Editor from "./Editor";
 import useLocalStorage from "./hooks/useLocalStorage";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faSave } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
 
@@ -29,22 +32,67 @@ function App() {
   const [initialSize, setInitialSize] = React.useState(null);
   
   const initial = (e) => {
-    // console.log("start");
     let resizable = document.getElementById('top');
     setInitialPos(e.clientY);
     setInitialSize(resizable.offsetHeight);
   }
   const resize = (e) => {
-    // console.log("resizing");
+    e.preventDefault();
     let resizable = document.getElementById('top');
     const newHeight = parseInt(initialSize) + parseInt(e.clientY - initialPos);
     resizable.style.height = newHeight > 100 && newHeight < window.innerHeight-100  ? newHeight + "px" : resizable.style.height
   }
 
+  ////clear
+  function clearSrcDoc(){
+    setHtml("")
+    setCss("")
+    setJs("")
+  }
+
+  ////download code
+  const [code , setCode] = useState("")
+  function downloadCode(){
+    if(html==="" && css === "" && js === "") return;
+    setCode(
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code with Navwe</title>
+    <style>
+      ${css}
+    </style>
+  </head>
+  <body>
+    ${html}
+    <script>
+      ${js}
+    </script>
+  </body>
+</html>`
+    )
+
+    const blob = new Blob([code], { type: "text/html" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.download = "code.html";
+    link.href = url;
+    link.click();
+  }
 
   return (
     <div className="parent">
-      <div className="pane top-pane" id="top">
+      <div className="header">
+        <button id="clr" className="icon" onClick={clearSrcDoc}>
+          <FontAwesomeIcon icon={faTrash} />
+        </button>
+        <button id="save" className="icon" onClick={downloadCode}>
+          <FontAwesomeIcon icon={faSave} />
+        </button>
+      </div>
+      <div className="pane top-pane" id="top" resizable='true'>
         <Editor 
           language = "xml"
           displayName = "HTML"
@@ -72,7 +120,7 @@ function App() {
         onDrag      = {resize}
       ></div>
 
-      <div className="pane" id="bottom">
+      <div className="pane" id="bottom" resizable='true'>
         <iframe 
           srcDoc={srcDoc}
           title="output"
